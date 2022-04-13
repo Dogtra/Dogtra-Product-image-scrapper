825d6c8360f116a6f605a9305b763805f224e9d9
JH Yang <yang@jhyang.xyz>
2022-04-12 17:36:48 -0700

    Removed images & added all product downloads

--- index.js
+++ index.js
@@ -3,14 +3,33 @@ let request = require('request');
 const { JSDOM } = require("jsdom")
 const axios = require('axios')
 
-const download = (uri, filename, callback) => {
-    request.head(uri, function(err, res, body){
-      console.log('content-type:', res.headers['content-type']);
-      console.log('content-length:', res.headers['content-length']);
-  
-      request(uri).pipe(fs.createWriteStream('images/' + filename)).on('close', callback);
-    });
-  };
+const download_image = (url, filename) =>
+axios({
+  url,
+  responseType: 'stream',
+}).then(
+  response => 
+    new Promise((resolve, reject) => {
+
+      switch (response.headers['content-type']) {
+        case 'image/\png':
+          filename += '.png'
+          break;
+
+        case 'image/\jpeg':
+          filename += '.jpg'
+          break;
+        default: 
+          console.error('Content-type ' + response.headers['content-type'] + ' is not handled')
+          return
+      }
+
+      response.data
+        .pipe(fs.createWriteStream('images/' + filename))
+        .on('finish', () => resolve())
+        .on('error', e => reject(e));
+    }),
+);
 
 const getProducts = async () => {
     htmlSource = fs.readFileSync("product_list.html", "utf8");
@@ -24,29 +43,37 @@ const getProducts = async () => {
     console.log("Fetching product list")
 
     document.querySelectorAll('a[id=desktop]').forEach(item => {
-        getImages(item['href'])
-
+      getImages(item['href'])
     })
 }
 
 const getImages = async (uri) => {
 
     const { data } = await axios.get(uri);
+
     const dom = new JSDOM(data, {
       resources: "usable"
     });
+
     const { document } = dom.window;
-    
-    const productName = document.getElementsByClassName('pdp-header-title')[0].innerHTML.replace(/ /g,"_");
+
+    const productName = document.getElementsByClassName('pdp-header-title')[0]
+    .innerHTML
+    .replace(/ /g,"_")
+    .replace(/[/\\?%*:|"<>]/g, '-');
+
+    console.log("Fetching images for " + productName)
 
     let count = 0;
 
-    Array.from(document.getElementById('productImages').childNodes).filter(item => item.tagName=="IMG").forEach(item =>  {
+    await Array.from(document.getElementById('productImages').childNodes)
+    .filter(item => item.tagName=="IMG")
+    .forEach(async item =>  {
         count++ 
-        download('https://dogtra.com' + item.src, productName + '_' + count + '.jpg', () => console.log('done'))
+        await download_image('https://dogtra.com' + item.src, productName + '_' + count, () => {})
     });
 }
 
 
 getProducts()
-// getImages()
+// getImages('https://www.dogtra.com/products/e-collars/tom-davis-280c')
